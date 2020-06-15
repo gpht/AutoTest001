04dfb7485ec5bc48e9cb6cee861fd3645a7e5be3
gpht <424277539@qq.com>
2020-06-15 13:18:56 +0800

    修改allTest文件，生成测试报告

--- AutoTest/testCase/allTest.py
+++ AutoTest/testCase/allTest.py
@@ -1,18 +1,41 @@
 
 import unittest
 import os
+import time
+import HTMLTestRunner
+
 
 def allCases():
+
     '''
     获取所有测试模块
     :return:
     '''
-    suite = unittest.TestLoader().discover(
-        start_dir = os.path.dirname(__file__),
-        pattern = 'test_*.py',
-        top_level_dir = None
+
+    suite = unittest.defaultTestLoader.discover(
+        start_dir=os.path.dirname(__file__),
+        pattern='test_*.py',
+        top_level_dir=None
     )
     return suite
 
+
+def getNowTime():
+    '''获取当前的时间'''
+    return time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+
+
+def run():
+    fileName = os.path.join(os.path.dirname(__file__), 'report', getNowTime()+'_report.html')
+    fp = open(fileName, 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+        stream=fp,
+        title='UI自动化测试报告',
+        description='UI自动化测试报告详细信息'
+    )
+    runner.run(allCases())
+    fp.close()
+
+
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(allCases())
+    run()
